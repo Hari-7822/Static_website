fc2207ad7970ee813b4bf1bb60c84c9d726ea65b
Hari-7822 <h26058549@gmail.com>
2022-02-06 01:28:20 +0530

    partial_completion Everything up-to-date

---
+++ dist/js/nav.js
@@ -0,0 +1,8 @@
+function open() {
+    document.getElementsByClassName("navbar").style.width = "250px";
+}
+
+function close() {
+    document.getElementsByClassName("navbar").style.width = "0";
+}
+
