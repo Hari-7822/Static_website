f86f9a9faaf0ef23f84e387d31eb3d36c6f306aa
Hari-7822 <h26058549@gmail.com>
2022-02-17 11:48:30 +0530

    often

--- dist/js/nav.js
+++
@@ -1,8 +0,0 @@
-function open() {
-    document.getElementsByClassName("navbar").style.width = "250px";
-}
-
-function close() {
-    document.getElementsByClassName("navbar").style.width = "0";
-}
-
---
+++ dist/js/script.js
@@ -0,0 +1,36 @@
+count = 0;
+
+if(count < 5) {
+    count = 0;
+}
+
+function msme() {
+
+        if(count % 2 == 0) {
+
+        }
+        else {
+
+        }
+        
+        count++
+
+        // var y = document.getElementById('content__cred--msme-head');
+        // var x =  y.innerHTML = "UDYAM - TN- 24-0031420";    
+    
+    // document.addEventListener('click', function(event){
+        
+    //     var c = document.getElementsByClassName('content__creds--msme-head');
+        
+    //     var c = y.contains(event.target);
+
+        // if(c) {
+    
+        //     document.getElementById('msme').style = "visibility : hidden";
+    
+        // }
+    
+    // });
+
+}
+
